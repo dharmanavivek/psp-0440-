125c581b7fa6686589d9c35dd7d53212afc29033
dharmanavivek <93912822+dharmanavivek@users.noreply.github.com>
2021-11-26 10:19:36 +0530

    Add files via upload

---
+++ vek.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h> 
+int main()
+{ int a=10;
+int b=4;
+float c=4.0;
+float d=2.0;
+int e,f;
+float g,h;
+e=a+b;
+printf("%d",e);
+f=a-b;
+printf(" %d",f);
+g=c+d;
+printf("\n%0.1f",g);
+h=c-d;
+printf(" %0.1f",h);
+ return 0;
+}
