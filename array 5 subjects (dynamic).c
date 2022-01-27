3fad2b79385ee1e12411cd016ccf937cab0fb2b6
dharmanavivek <93912822+dharmanavivek@users.noreply.github.com>
2022-01-27 21:00:21 +0530

    Create array 5 subjects (dynamic).c

---
+++ array 5 subjects (dynamic).c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    float marks[5];
+    printf("enter the physics marks");
+    scanf("%f",&marks[0]);
+     printf("\nenter the maths marks");
+    scanf("%f",&marks[1]);
+     printf("\nenter the chemistry marks");
+    scanf("%f",&marks[2]);
+     printf("\nenter the english marks");
+    scanf("%f",&marks[3]);
+     printf("\nenter the sanskrit marks");
+    scanf("%f",&marks[4]);
+    printf("%f\n",marks[4]);
+    printf("%f\n",marks[2]);
+}
